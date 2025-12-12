b8593cea364f157618739f3562d3eef86a37810e
HleyHub <hashleyed@gmail.com>
2025-12-11 20:28:37 -0500

    Day 2 query

---
+++ Day 2/day_2.sql
@@ -0,0 +1,23 @@
+SELECT * 
+FROM snowball_categories;
+
+SELECT * 
+FROM snowball_inventory;
+
+-- Using the snowball_inventory and snowball_categories tables, write a query that 
+-- returns valid snowball categories with the count of valid snowballs per category. 
+-- Your final table should have the columns official_category and total_usable_snowballs. 
+-- Sort the output from fewest to most total_usable_snowballs.
+
+SELECT 
+	sc.official_category,
+	COUNT(si.id) AS total_usable_snowballs
+FROM snowball_categories sc
+LEFT JOIN snowball_inventory si
+	ON sc.official_category = si.category_name
+WHERE si.status = 'ready'  -- I could also use AND si.status = 'ready' and get the same result.
+GROUP BY sc.official_category
+ORDER BY total_usable_snowballs ASC;
+
+-- Notice that here I am joining with the category name because there is no numeric foreign key.	
+
