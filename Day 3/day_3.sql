2ef35cff21aea3fc2fe7cddaa0b1ee77d9e152ae
HleyHub <hashleyed@gmail.com>
2025-12-14 11:43:41 -0500

    updated day_3 and Day 5 queries

--- Day 3/day_3.sql
+++ Day 3/day_3.sql
@@ -1,33 +1,19 @@
 SELECT *
 FROM hotline_messages;
 
--- Checking for records with the word "sorry"
-SELECT *
-FROM hotline_messages
-WHERE transcript LIKE '%sorry%'
-; -- This returned 104 records.
-
--- Updating records with the word "sorry" in the transcript to an approved status.
 UPDATE hotline_messages
 SET status = 'approved'
 WHERE transcript LIKE '%sorry%'
 ;
 
--- Checking for records with the tags "penguin prank", "time-loop advisory", "possible dragon", 
--- or "nonsense alert" or if the caller's name is "Test Caller"
-SELECT *
-FROM hotline_messages
-WHERE tag IN ('penguin prank', 'time-loop advisory', 'possible dragon', 'nonsense alert')
-	OR caller_name = 'Test Caller'
-; -- This returned 89 records.
+DELETE F hotline_messages
+SET status = 'approved'
+WHERE transcript LIKE '%sorry%'
+;
 
--- Deleting for records with the tags "penguin prank", "time-loop advisory", "possible dragon", 
--- or "nonsense alert" or if the caller's name is "Test Caller"
-DELETE FROM hotline_messages
-WHERE tag IN ('penguin prank', 'time-loop advisory', 'possible dragon', 'nonsense alert')
-	OR caller_name = 'Test Caller'
-; -- This deleted 89 records
-
+WITH cleaned_messages AS (
+	
+)
 SELECT 
 	COUNT(
 		CASE
@@ -38,4 +24,4 @@ SELECT
 			WHEN status IS NULL THEN 1
 		END ) AS rev_mess
 FROM hotline_messages
-; -- This returned 477 approved messages and 501 that needs to be reviewed.
+;
---
+++ Day 5/day_5.sql
@@ -0,0 +1,30 @@
+SELECT *
+FROM listening_logs;
+
+WITH playlist AS (
+	SELECT 
+		user_name,
+		artist,
+		COUNT(*) AS most_played
+	FROM listening_logs
+	GROUP BY user_name, artist
+),
+ranked_artists AS (
+	SELECT 
+		user_name,
+		artist,
+		most_played,
+		ROW_NUMBER() OVER (
+			PARTITION BY user_name, artist 
+			ORDER BY most_played DESC
+		) AS artist_ranking
+	FROM playlist
+)
+SELECT 
+	user_name,
+	artist,
+	most_played
+FROM ranked_artists
+WHERE artist_ranking <= 3
+ORDER BY most_played DESC
+;
