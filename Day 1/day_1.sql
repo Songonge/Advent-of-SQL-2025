d6885f2c8cdd2440de49185fc58cecfe8cc56b02
HleyHub <hashleyed@gmail.com>
2025-12-15 09:28:55 -0500

    updated day_1 query

--- Day 1/day_1.sql
+++ Day 1/day_1.sql
@@ -1,3 +1,9 @@
+-- Retrieving all the data from the table
+
+SELECT *
+FROM wish_list;
+
+-- ===================================== THE CHALLENGE =====================================
 -- Using the wish_list table, count how many times each cleaned toy name appears, 
 -- from most requested to least requested. Return the results in two columns: wish and count. 
 -- Make sure the wish results have no extra leading or trailing spaces and are all lowercase.
