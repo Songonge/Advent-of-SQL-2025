40d13541b649bc8428efda2b08123113f0ee79d1
HleyHub <hashleyed@gmail.com>
2025-12-15 09:29:39 -0500

    updated day_3 query

--- Day 3/day_3.sql
+++ Day 3/day_3.sql
@@ -1,5 +1,19 @@
+-- Retrieving all data from the table
 SELECT *
 FROM hotline_messages;
+
+-- ============================ THE CHALLENGE ===========================
+-- Using the hotline_messages table, update any record that has 
+-- "sorry" (case insensitive) in the transcript and doesn't 
+-- currently have a status assigned to have a status of "approved".
+
+-- Then delete any records where the tag is "penguin prank", 
+-- "time-loop advisory", "possible dragon", or "nonsense alert" 
+-- or if the caller's name is "Test Caller".
+
+-- After updating and deleting the records as described, write a final 
+-- query that returns how many messages currently have a status of 
+-- "approved" and how many still need to be reviewed (i.e., status is NULL).
 
 UPDATE hotline_messages
 SET status = 'approved'
